e9e554e5a1fae38826938284db2095909c84479a
Stanislav Utikeev <utikeev@gmail.com>
2020-05-19 23:52:15 +0300

    [SieveBased] Use sets in terminology maps (it's actually important)

--- common/SieveBased/processing/terminology.py
+++ common/SieveBased/processing/terminology.py
@@ -25,14 +25,14 @@ class Terminology:
         """
         self.terminology_path: str = terminology_path
         self.text_processor: TextProcessor = text_processor
-        self.name_to_cui_map: Dict[str, List[str]] = defaultdict(list)
-        self.cui_to_name_map: Dict[str, List[str]] = defaultdict(list)
-        self.stemmed_name_to_cui_map: Dict[str, List[str]] = defaultdict(list)
-        self.cui_to_stemmed_name_map: Dict[str, List[str]] = defaultdict(list)
+        self.name_to_cui_map: Dict[str, Set[str]] = defaultdict(set)
+        self.cui_to_name_map: Dict[str, Set[str]] = defaultdict(set)
+        self.stemmed_name_to_cui_map: Dict[str, Set[str]] = defaultdict(set)
+        self.cui_to_stemmed_name_map: Dict[str, Set[str]] = defaultdict(set)
         self.token_to_name_map: Dict[str, Set[str]] = defaultdict(set)
-        self.normalized_name_to_cui_map: Dict[str, List[str]] = defaultdict(list)
-        self.stemmed_normalized_name_to_cui_map: Dict[str, List[str]] = defaultdict(list)
-        self.simple_name_to_cui_map: Dict[str, List[str]] = defaultdict(list)
+        self.normalized_name_to_cui_map: Dict[str, Set[str]] = defaultdict(set)
+        self.stemmed_normalized_name_to_cui_map: Dict[str, Set[str]] = defaultdict(set)
+        self.simple_name_to_cui_map: Dict[str, Set[str]] = defaultdict(set)
 
     def load_data(self, *, verbose: bool = False):
         """Loads data in terminology.
@@ -48,15 +48,17 @@ class Terminology:
         aliases = aliases_str.lower().split('|')
         for alias in aliases:
             self._put_to_maps(cui, alias)
-            self._put_to_maps(cui, alias.replace(',', ''))
+            cleaned_alias = alias.replace(',', '')
+            if cleaned_alias != alias:
+                self._put_to_maps(cui, alias.replace(',', ''))
 
     def _put_to_maps(self, cui: str, alias: str):
-        self.name_to_cui_map[alias].append(cui)
-        self.cui_to_name_map[cui].append(alias)
+        self.name_to_cui_map[alias].add(cui)
+        self.cui_to_name_map[cui].add(alias)
 
         stemmed_concept_name = self.text_processor.get_stemmed_phrase(alias)
-        self.stemmed_name_to_cui_map[stemmed_concept_name].append(cui)
-        self.cui_to_stemmed_name_map[cui].append(stemmed_concept_name)
+        self.stemmed_name_to_cui_map[stemmed_concept_name].add(cui)
+        self.cui_to_stemmed_name_map[cui].add(stemmed_concept_name)
 
         tokens = alias.split()
         for token in tokens:
@@ -65,9 +67,9 @@ class Terminology:
 
         if len(tokens) == 3:
             new_phrase = f'{tokens[0]} {tokens[2]}'
-            self.simple_name_to_cui_map[new_phrase].append(cui)
+            self.simple_name_to_cui_map[new_phrase].add(cui)
             new_phrase = f'{tokens[1]} {tokens[2]}'
-            self.simple_name_to_cui_map[new_phrase].append(cui)
+            self.simple_name_to_cui_map[new_phrase].add(cui)
 
     def store_normalized_entity(self, entity: SieveBasedEntity):
         """Store already normalized entity to have it in cache.
