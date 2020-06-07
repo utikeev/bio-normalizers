3f70bb50b69dc2eb8518e21c6eb4fbd42f88dc02
Stanislav Utikeev <utikeev@gmail.com>
2020-06-07 15:39:16 +0300

    Fix data

--- setup.py
+++ setup.py
@@ -4,8 +4,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='bionorm',
-    version='0.0.7',
+    version='0.0.8',
     packages=find_packages(),
     install_requires=['tqdm', 'nltk'],
+    include_package_data=True,
+    package_data={
+        'bionorm': ['bionorm/common/SieveBased/data/*.txt'],
+    },
     long_description=open(join(dirname(__file__), 'README.md')).read()
 )
