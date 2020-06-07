1011ceeb25c1f906d742af9bea5009e7330fe986
Stanislav Utikeev <utikeev@gmail.com>
2020-06-07 15:16:14 +0300

    Dump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bionorm',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     install_requires=['tqdm', 'nltk'],
     long_description=open(join(dirname(__file__), 'README.md')).read()
